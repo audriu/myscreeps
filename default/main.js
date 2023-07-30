4314e5357947f7d60913d393efe72c59b55da80b
Audrius Molis <vienas00@gmail.com>
2023-07-30 13:04:57 +0300

    Refactor main module

--- default/main.js
+++ default/main.js
@@ -8,72 +8,52 @@ const tasks = require('tasks');
 const towers = require('towers');
 const spawning = require('./spawning');
 
-loop = function () {
-    const diena = Memory.diena ? Memory.diena : 1
-    Memory.diena = (diena > 6) ? 1 : diena + 1
-
-    switch (diena) {
-        case 1:
-            console.log('Pirmadienis - kambariu rekonfiguracija' + JSON.stringify(Game.rooms));
-            Memory.rooms = Object.keys(Game.rooms);
-            break;
-        case 2:
-            console.log('Antradienis');
-            break;
-        case 6:
-            Memory.rooms.forEach(spawning_.handle_spawning);
-            spawning_.delete_dead_creeps();
-            break;
-        default:
-    }
-
-    towers();
-
-    for (const name in Game.creeps) {
-        const creep = Game.creeps[name];
-        const role = creep.memory.role;
-
-        if (creep.memory.harvesting && (creep.memory.targetRoom && creep.room.name != creep.memory.targetRoom)) {
-            tasks.goToYourRoom(creep);
-        } else if (creep.memory.task === 'renewing') {
-            tasks.renew(creep);
-        } else if (role === 'harvester') {
-            roleHarvester.run(creep);
-        } else if (role === 'upgrader') {
-            roleUpgrader.run(creep);
-        } else if (role === 'builder') {
-            roleBuilder.run(creep);
-        } else if (role === 'colonist' && role !== 'fighter') {
-            roleColonist.run(creep);
-        } else if (role === 'old') {
-            tasks.old(creep);
-        } else if (role === 'fighter') {
-            roleFighter.run(creep);
-        }
-
-        if (creep.store[RESOURCE_ENERGY] === 0 && creep.ticksToLive > 100) {
-            creep.memory.harvesting = true;
-        } else if (creep.store[RESOURCE_ENERGY] === 0 && creep.ticksToLive < 100) {
-            creep.suicide();
-        }
-    }
-}
-
-hi = function () {
-    console.log("hi!!!")
-}
-
 module.exports = {
-    hi: hi,
-    loop: loop
+    loop: function () {
+        const diena = Memory.diena ? Memory.diena : 1
+        Memory.diena = (diena > 6) ? 1 : diena + 1
+        console.log('Diena: ' + Memory.diena);
+
+        switch (diena) {
+            case 1:
+                Memory.rooms = Object.keys(Game.rooms);
+                break;
+            case 6:
+                Memory.rooms.forEach(spawning_.handle_spawning);
+                spawning_.delete_dead_creeps();
+                break;
+            default:
+        }
+
+        towers();
+
+        for (const name in Game.creeps) {
+            const creep = Game.creeps[name];
+            const role = creep.memory.role;
+
+            if (creep.memory.harvesting && (creep.memory.targetRoom && creep.room.name != creep.memory.targetRoom)) {
+                tasks.goToYourRoom(creep);
+            } else if (creep.memory.task === 'renewing') {
+                tasks.renew(creep);
+            } else if (role === 'harvester') {
+                roleHarvester.run(creep);
+            } else if (role === 'upgrader') {
+                roleUpgrader.run(creep);
+            } else if (role === 'builder') {
+                roleBuilder.run(creep);
+            } else if (role === 'colonist' && role !== 'fighter') {
+                roleColonist.run(creep);
+            } else if (role === 'old') {
+                tasks.old(creep);
+            } else if (role === 'fighter') {
+                roleFighter.run(creep);
+            }
+
+            if (creep.store[RESOURCE_ENERGY] === 0 && creep.ticksToLive > 100) {
+                creep.memory.harvesting = true;
+            } else if (creep.store[RESOURCE_ENERGY] === 0 && creep.ticksToLive < 100) {
+                creep.suicide();
+            }
+        }
+    }
 }
-
-//Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Builder1', { memory: { role: 'builder' } });
-//Game.spawns['Spawn1'].room.createConstructionSite(23, 22, STRUCTURE_TOWER);
-//Game.spawns['Spawn1'].room.controller.activateSafeMode();
-
-// for (const name in Game.rooms) {
-//     console.log('Room "' + name + '" has ' + Game.rooms[name].energyAvailable + ' energy');
-// }
-
-//OBSTACLE_OBJECT_TYPES: ["spawn", "creep", "powerCreep", "source", "mineral", "deposit", "controller", "constructedWall", "extension", "link", "storage", "tower", "observer", "powerSpawn", "powerBank", "lab", "terminal", "nuker", "factory", "invaderCore"]
