df532d46c7dd3004cf4c5499e3465641a28a518c
Audrius Molis <vienas00@gmail.com>
2023-07-30 12:29:41 +0300

    refactor towers behaviour

--- default/towers.js
+++ default/towers.js
@@ -1,48 +1,17 @@
-function attack(tower) {
-    const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if (closestHostile)
-        tower.attack(closestHostile);
-}
-
-function heal(tower) {
-    const wounded = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
-        filter: (creep) => {
-            return (creep.hits < creep.hitsMax);
-        }
-    });
-    if (wounded)
-        tower.heal(wounded);
-}
-
-function repair(tower) {
-    const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: (structure) => structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL
-    });
-    if (closestDamagedStructure)
-        tower.repair(closestDamagedStructure);
-}
-
 module.exports = function () {
     Memory.rooms.forEach(room => {
-        console.log( "------"+ room);
-        console.log(Game.rooms.W34S42);
-        console.log(Game.rooms[room]);
+        const towers = Game.rooms[room].find(FIND_STRUCTURES, { filter: (str) => str.structureType == STRUCTURE_TOWER });
+        towers.forEach(tower => {
+            const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, { filter: (structure) => structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL });
+            const wounded = tower.pos.findClosestByRange(FIND_MY_CREEPS, { filter: (creep) => { return (creep.hits < creep.hitsMax); } });
+            if (closestHostile) {
+                tower.attack(closestHostile);
+            } else if (wounded) {
+                tower.heal(wounded);
+            } else if (closestDamagedStructure) {
+                tower.repair(closestDamagedStructure);
+            }
+        });
     });
-
-    var towers1 = Game.rooms.W34S42.find(FIND_STRUCTURES, { filter: (str) => str.structureType == STRUCTURE_TOWER });
-    var towers2 = Game.rooms.W33S42.find(FIND_STRUCTURES, { filter: (str) => str.structureType == STRUCTURE_TOWER });
-    var towers3 = Game.rooms.W33S42.find(FIND_STRUCTURES, { filter: (str) => str.structureType == STRUCTURE_TOWER });
-    var towers4 = Game.rooms.W32S41.find(FIND_STRUCTURES, { filter: (str) => str.structureType == STRUCTURE_TOWER });
-    var towers5 = Game.rooms.W33S41.find(FIND_STRUCTURES, { filter: (str) => str.structureType == STRUCTURE_TOWER });
-    var towers6 = Game.rooms.W32S43.find(FIND_STRUCTURES, { filter: (str) => str.structureType == STRUCTURE_TOWER });
-    var towers7 = Game.rooms.W33S43.find(FIND_STRUCTURES, { filter: (str) => str.structureType == STRUCTURE_TOWER });
-    let towers = towers1.concat(towers2).concat(towers3).concat(towers4).concat(towers5).concat(towers6).concat(towers7)
-    towers = _.shuffle(towers);
-    const attackers = towers;
-    const healers = [towers[0]];
-    const repairers = [towers[1], towers2[0], towers3[0], towers4[0], towers5[0], towers6[0], towers7[0]];
-
-    healers.forEach(heal);
-    repairers.forEach(repair);
-    attackers.forEach(attack);
 }
