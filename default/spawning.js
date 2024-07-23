a8c2330173f691bb6ea1350f378fa4ce1979fba9
Audrius Molis <vienas00@gmail.com>
2024-07-23 21:03:08 +0300

    _

--- default/spawning.js
+++ default/spawning.js
@@ -43,7 +43,7 @@ const room_configs = {
             'ant': 4
         }
     },
-    'E41S59_': {
+    'E41S59': {
         default_spawn: 'Spawn1',
         body_cost: 400,
         body_plan: [MOVE, WORK, CARRY, MOVE, WORK, CARRY],
