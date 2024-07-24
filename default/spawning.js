7d0f7a28d5cd9f7b2c1039d0d1eb675f6a438cf5
Audrius Molis <vienas00@gmail.com>
2024-07-24 13:34:17 +0300

    _

--- default/spawning.js
+++ default/spawning.js
@@ -67,24 +67,24 @@ const room_configs = {
             'ant': 5
         }
     }
-    ,
-    'E44S61': {
-        default_spawn: 'Spawn3',
-        body_cost: 200,
-        body_plan: [MOVE, WORK, CARRY],
-        contingent: {
-            'ant': 2
-        }
-    }
-    ,
-    'E43S61': {
-        default_spawn: 'Spawn3',
-        body_cost: 200,
-        body_plan: [MOVE, WORK, CARRY],
-        contingent: {
-            'ant': 2
-        }
-    }
+    // ,
+    // 'E44S61': {
+    //     default_spawn: 'Spawn3',
+    //     body_cost: 200,
+    //     body_plan: [MOVE, WORK, CARRY],
+    //     contingent: {
+    //         'ant': 2
+    //     }
+    // }
+    // ,
+    // 'E43S61': {
+    //     default_spawn: 'Spawn3',
+    //     body_cost: 200,
+    //     body_plan: [MOVE, WORK, CARRY],
+    //     contingent: {
+    //         'ant': 2
+    //     }
+    // }
 }
 
 module.exports = {
