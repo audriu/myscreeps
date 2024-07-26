938560b92b6c45aa8016bcf97549bc230d3d177d
Audrius Molis <vienas00@gmail.com>
2024-07-26 11:37:21 +0300

    _

--- default/spawning.js
+++ default/spawning.js
@@ -78,12 +78,11 @@ module.exports = {
                     console.log('Spawning new ' + newName + ' in ' + roomName);
                     theSpawn.spawnCreep(_.shuffle(config.body_plan), newName, { memory: memory });
                 } else if (energy_available >= fighterBodyCost) {
-                    console.log('Spawning new fighter');
                     const spawnsInRoom = Object.values(Game.spawns).filter(spawn => spawn.room.name === roomName);
                     for (const spawn of spawnsInRoom) {
-                        spawn.spawnCreep(_.shuffle(fighterBodyPlan), 'badass' + Game.time + spawn, { memory: { role: "fighter", targetRoom: 'W32S46' } });
+                        const spawnRes = spawn.spawnCreep(_.shuffle(fighterBodyPlan), 'badass' + Game.time + spawn, { memory: { role: "fighter", targetRoom: 'W14N56' } });
+                        console.log('Spawning new fighter ' + spawnRes);
                     }
-
                 }
             });
 
