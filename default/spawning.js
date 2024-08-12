b92a65e694c35c69a8fcb85bd56538f1849b9c7d
Audrius Molis <vienas00@gmail.com>
2024-08-12 22:14:25 +0300

    _

--- default/spawning.js
+++ default/spawning.js
@@ -32,16 +32,16 @@ const room_configs = {
     }
     , "W31N16": {
         default_spawn: 'Spawn2',
-        body_cost: 400,
-        body_plan: [MOVE, WORK, CARRY, MOVE, WORK, CARRY],
+        body_cost: antBodyCost,
+        body_plan: antBodyPlan,
         contingent: {
             'ant': 6
         }
     }
     , "W31N17": {
         default_spawn: 'Spawn3',
-        body_cost: 400,
-        body_plan: [MOVE, WORK, CARRY, MOVE, WORK, CARRY],
+        body_cost: antBodyCost,
+        body_plan: antBodyPlan,
         contingent: {
             'ant': 6
         }
@@ -65,16 +65,16 @@ const room_configs = {
     }
     , "W32N14": {
         default_spawn: 'Spawn6',
-        body_cost: 400,
-        body_plan: [MOVE, WORK, CARRY, MOVE, WORK, CARRY],
+        body_cost: antBodyCost,
+        body_plan: antBodyPlan,
         contingent: {
             'ant': 6
         }
     }
     , "W32N13": {
-        default_spawn: 'Spawn7',
-        body_cost: 400,
-        body_plan: [MOVE, WORK, CARRY, MOVE, WORK, CARRY],
+        default_spawn: 'Spawn6',
+        body_cost: 200,
+        body_plan: [MOVE, WORK, CARRY],
         contingent: {
             'ant': 6
         }
